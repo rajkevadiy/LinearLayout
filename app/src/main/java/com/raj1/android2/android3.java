b82463750ac2ff5152290aeeb7ac0b997e7b5685
Raj Kevadiya <kevadiyar98@gmail.com>
2022-11-19 14:37:56 +0530

    raj

---
+++ app/src/main/java/com/raj1/android2/android3.java
@@ -0,0 +1,14 @@
+package com.raj1.android2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class android3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android3);
+    }
+}
